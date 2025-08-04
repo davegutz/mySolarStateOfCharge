15e61932d415473552d89bd372d193c1219af990
Dave G <davegutz@alum.mit.edu>
2025-08-04 05:37:01 -0400

    Update CompareRunSim.py

--- SOC_Particle/pyStateOfCharge/CompareRunSim.py
+++ SOC_Particle/pyStateOfCharge/CompareRunSim.py
@@ -144,7 +144,7 @@ def compare_run_sim(data_file=None, unit_key=None, time_end_in=None, data_only=F
 
 
 def main():
-    data_file = 'G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20250612a/vv4 20250803am_soc4p2_hi_lo_bb.csv'
+    data_file = 'G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20250612a/vv4 20250804am_soc4p2_hi_lo_bb.csv'
     unit_key = 'g20250612a_soc4p2_hi_lo_bb'
     time_end_in = None
     # time_end_in = 40
