6b1aa6e51fd7e11da798f462f7e5c97c3c3581b5
Dave G <davegutz@alum.mit.edu>
2025-08-16 12:03:34 -0400

    Update serial.cpp

--- SOC_Particle/src/serial.cpp
+++ SOC_Particle/src/serial.cpp
@@ -190,7 +190,7 @@ void print_serial_temp_data(const boolean reset, Sensors *Sen)
   if ( sp.debug()==1  || sp.debug()==2  || sp.debug()==3 || sp.debug()==4  )
   {
     double cTime = double(Sen->now_temp)/1000.;
-    Serial.printf("temp_unit, %13.3f, %9.6f, %9.5f, %9.5f, %9.5f, %d, %9.5f, %9.5f, %9.5f, %9.5f,\n",
+    Serial.printf("temp_unit, %13.3f, %11.7f, %11.7f, %11.7f, %11.7f, %d, %11.7f, %11.7f, %11.7f, %11.7f,\n",
       cTime, Sen->T_temp, Sen->Tb_hdwe, Sen->Tb_model, Sen->Tb, reset, Sen->Tb_hdwe_filt, Sen->Tb_hdwe_filt_rate,
       Sen->TbSenseFilt->rstate(), Sen->TbSenseFilt->lstate());
     Log.info("    print_serial_temp_data cTime,%9.3f,", cTime);
