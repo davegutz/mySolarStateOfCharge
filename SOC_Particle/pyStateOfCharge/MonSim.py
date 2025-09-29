8c1c525f1d5d9b75dec5ec684c19931b2d4591a2
Dave G <davegutz@alum.mit.edu>
2025-09-29 08:12:44 -0400

    Update MonSim.py

--- SOC_Particle/pyStateOfCharge/MonSim.py
+++ SOC_Particle/pyStateOfCharge/MonSim.py
@@ -107,7 +107,7 @@ def replicate(mon_old, sim_old=None, init_time=-4., t_vb_fail=None, vb_fail=13.2
     16. *** Fig 10  EKF 3:  z=voc_stat_f and ver not equal.  Fixed by updating variable names
     17. *** Fig 12 Hyst 1: e_wrap_ver not equal.  Fixed by adding scale logic from app
     18. *** Fig 13 sim_s 1:  ib_in_ver  Not fixed: OK because have manually over-ridden ib selection to force ib_noa use in logic but not selection
-    19. Fig 15 sim_s 2a:  vb?   Keep looking for this when run at other op conditions
+    19. Fig 15 sim_s 2a:  vb?   Keep looking for this when run at other op conditions.  Shutdown problem.
     20. **** Fig. many:  delta_q_s_ver != delta_q_s   Fixed by changing Sen->T to t_ in Sim::count_coulombs
     """
 
