5a408df9fe16fd8abbc0bda031b194c78c70ea19
Dave G <davegutz@alum.mit.edu>
2025-12-06 12:09:45 -0500

    noise study in folder

--- SOC_Particle/pyStateOfCharge/noise study/lombscargleVc_VoOnly.py
+++ SOC_Particle/pyStateOfCharge/noise study/lombscargleVc_VoOnly.py
@@ -2,16 +2,29 @@ import numpy as np
 from scipy.signal import lombscargle
 import matplotlib.pyplot as plt
 
-# Obtain data by:  Cx2500 (Cx10000 for build of Vca only).  Save as 'burst'  Data is approximately 40 Hz so Nyquist is 20 Hz = 125 r/s
-# handedit file to remove any information above header row
+# Obtain data by:  Cx20000.  Save as 'burst'  Data is approximately 40 Hz so Nyquist is 20 Hz = 125 r/s
+# handedit file to remove any information above header row and below end
 # primary peak at 1.6 Hz excites sample mode resolution artifacts at 1.6 +  3.1*n
 
 # Define frequencies to test
 data_file_clean = 'burstVo_Vc_Cx20000_soc2p2_hi_lo_chg.csv'
+# data_file_clean = 'burstVo_Vc_Cx20000_BTremo_soc2p2_hi_lo_chg.csv'
+
+# Get data and statistics
 data_raw = np.genfromtxt(data_file_clean, delimiter=',', names=True, dtype=float).view(np.recarray)
-nyquist = 95./2.  # get this by looking at plots and rerunning
-numpts = len(data_raw)
-freqs_rps = np.linspace(0.1, nyquist*2*np.pi, 20000)
+sampt = []
+sampf = []
+for i in range(len(data_raw.time)):
+    if i == 0:
+        sampt.append(0.)
+        sampf.append(0.)
+    else:
+        sampt.append(data_raw.time[i] - data_raw.time[i-1])
+        sampf.append(1./(data_raw.time[i] - data_raw.time[i-1]))
+sample_freq_hz = np.average(sampf)
+nyquist = sample_freq_hz
+N = len(data_raw)
+freqs_rps = np.linspace(0.1, nyquist*2*np.pi, N)
 
 # Compute the Lomb-Scargle periodogram
 power_Vcn = lombscargle(data_raw.time, data_raw.Vcn, freqs_rps, floating_mean=True)
@@ -33,7 +46,7 @@ plt.plot(data_raw.time, data_raw.Vcn, color='green', linestyle='-', label='Vc no
 plt.legend(loc=1)
 plt.xlabel('Time, s')
 plt.ylabel('Volts')
-plt.title('Time History of Data Used Periodogram')
+plt.title('Time History of Data Used Periodogram ' + data_file_clean)
 plt.grid(True)
 
 plt.figure(figsize=(10, 6))
@@ -41,26 +54,15 @@ plt.plot(data_raw.time, data_raw.VoVcn, color='red', linestyle='--', label='Vo-V
 plt.legend(loc=1)
 plt.xlabel('Time, s')
 plt.ylabel('Volts')
-plt.title('Time History of Data Used Periodogram')
+plt.title('Time History of Data Used Periodogram ' + data_file_clean)
 plt.grid(True)
-
-sampt = []
-sampf = []
-for i in range(len(data_raw.time)):
-    if i == 0:
-        sampt.append(0.)
-        sampf.append(0.)
-    else:
-        sampt.append(data_raw.time[i] - data_raw.time[i-1])
-        sampf.append(1./(data_raw.time[i] - data_raw.time[i-1]))
-
 
 plt.figure(figsize=(10, 6))
 plt.plot(data_raw.time, sampt, color='green', linestyle='-', label='Vc Sample Time, sec') # Convert to Hz for x-axis
 plt.legend(loc=1)
 plt.xlabel('Time, s')
 plt.ylabel('sec')
-plt.title('Time History of Sample Time Data Used Periodogram')
+plt.title('Time History of Sample Time Data Used Periodogram ' + data_file_clean)
 plt.grid(True)
 
 plt.figure(figsize=(10, 6))
@@ -68,7 +70,7 @@ plt.plot(data_raw.time, sampf, color='green', linestyle='-', label='Sample Vc fr
 plt.legend(loc=1)
 plt.xlabel('Time, s')
 plt.ylabel('Hz')
-plt.title('Time History of Sample Frequency Data Used Periodogram')
+plt.title('Time History of Sample Frequency Data Used Periodogram ' + data_file_clean)
 plt.grid(True)
 
 # Periodograms
@@ -77,7 +79,7 @@ plt.semilogx(freqs_rps / (2 * np.pi), power_Vcn, color='blue', linestyle='--', l
 plt.legend(loc=1)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power')
-plt.title('Lomb-Scargle Periodogram')
+plt.title('Lomb-Scargle Periodogram ' + data_file_clean)
 plt.grid(True)
 
 plt.figure(figsize=(10, 6))
@@ -85,7 +87,7 @@ plt.semilogx(freqs_rps / (2 * np.pi), power_Von, color='magenta', linestyle='-',
 plt.legend(loc=1)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power')
-plt.title('Lomb-Scargle Periodogram')
+plt.title('Lomb-Scargle Periodogram ' + data_file_clean)
 plt.grid(True)
 
 plt.figure(figsize=(10, 6))
@@ -93,16 +95,16 @@ plt.semilogx(freqs_rps / (2 * np.pi), power_Vo_Vcn, color='magenta', linestyle='
 plt.legend(loc=1)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power')
-plt.title('Lomb-Scargle Periodogram')
+plt.title('Lomb-Scargle Periodogram ' + data_file_clean)
 plt.grid(True)
 
 plt.figure(figsize=(10, 6))
 plt.plot(freqs_rps / (2 * np.pi), power_Vcn, color='blue', linestyle='--', label='Vc amp at ADC, Volts') # Convert to Hz for x-axis
-plt.xlim([1, 25])
+plt.xlim([1, nyquist])
 plt.legend(loc=1)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power')
-plt.title('Lomb-Scargle Periodogram')
+plt.title('Lomb-Scargle Periodogram ' + data_file_clean)
 plt.grid(True)
 
 plt.show()
