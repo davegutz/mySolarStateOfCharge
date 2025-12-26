60a96ca3d37db4b44d07f0a9397211372bb7f692
Dave G <davegutz@alum.mit.edu>
2025-12-26 03:55:52 -0500

    Update ble-uart-peripheral.cpp

--- ble-uart-peripheral/src/ble-uart-peripheral.cpp
+++ ble-uart-peripheral/src/ble-uart-peripheral.cpp
@@ -17,9 +17,8 @@ const BleUuid txUuid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
 BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
 BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
 
-void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
-    // Log.trace("Received data from: %02X:%02X:%02X:%02X:%02X:%02X:", peer.address()[5], peer.address()[4], peer.address()[3], peer.address()[2], peer.address()[1], peer.address()[0]);
-
+void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
     for (size_t ii = 0; ii < len; ii++) {
         Serial.write(data[ii]);
     }
@@ -29,10 +28,8 @@ void setup() {
     Serial.begin();
 
 	BLE.on();
-
     BLE.addCharacteristic(txCharacteristic);
     BLE.addCharacteristic(rxCharacteristic);
-
     BleAdvertisingData data;
     data.appendServiceUUID(serviceUuid);
     BLE.advertise(&data);
@@ -45,7 +42,6 @@ void loop() {
 
     	while(Serial.available() && txLen < UART_TX_BUF_SIZE) {
             txBuf[txLen++] = Serial.read();
-            Serial.write(txBuf[txLen - 1]);
         }
         if (txLen > 0) {
             txCharacteristic.setValue(txBuf, txLen);
