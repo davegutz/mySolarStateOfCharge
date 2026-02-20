211345ead246c93859d20c6da94b0c8342344023
Dave G <davegutz@alum.mit.edu>
2026-02-20 15:19:34 -0500

    Update ComparePlotSettings.py

--- SOC_Particle/pyStateOfCharge/ComparePlotSettings.py
+++ SOC_Particle/pyStateOfCharge/ComparePlotSettings.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
 plt.rcParams['axes.grid'] = True
 plt.rcParams['legend.fontsize'] = 'small'
-plt.rcParams['figure.figsize'] = [16, 9]  # 16:9 full-screen aspect ratio
 plt.rcParams['savefig.dpi'] = 300  # Set default savefig DPI to 300
 # plt.rcParams['figure.dpi'] = 300  # Also increase display DPI for consistency
+
+# Monkey-patch savefig: resize to 16:9 for the save, restore display size after.
+_orig_savefig = plt.savefig
+
+def _savefig_fullscreen(*args, **kwargs):
+    fig = plt.gcf()
+    orig_size = fig.get_size_inches()
+    fig.set_size_inches(16, 9)
+    _orig_savefig(*args, **kwargs)
+    fig.set_size_inches(*orig_size)
+
+plt.savefig = _savefig_fullscreen
